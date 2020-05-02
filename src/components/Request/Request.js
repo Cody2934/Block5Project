9f4a3338d263d6b5625a9ee383a7f7dacc72eb54
Cody Brown <cody6lefever@gmail.com>
2020-05-02 15:54:58 -0700

    container and component

---
+++ src/components/Request/Request.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Request.css';
+
+const Request = (url, onUrlChange, body, onBodyChange, onMethodChange, onSubmit) => (
+  <form className={styles.Request} onSubmit={onSubmit}>
+    <input type="text" value={url} onChange={onUrlChange} />
+
+    <input id="POST" type="radio" name="method" value="POST" onChange={onMethodChange} />
+    <label htmlFor="POST">POST</label>
+
+    <input id="GET" type="radio" name="method" value="GET" onChange={onMethodChange} />
+    <label htmlFor="GET">GET</label>
+
+    <input id="PATCH" type="radio" name="method" value="PATCH" onChange={onMethodChange} />
+    <label htmlFor="PATCH">PATCH</label>
+
+    <input id="PUT" type="radio" name="method" value="PUT" onChange={onMethodChange} />
+    <label htmlFor="PUT">PUT</label>
+
+    <input id="DELETE" type="radio" name="method" value="DELETE" onChange={onMethodChange} />
+    <label htmlFor="DELETE">DELETE</label>
+
+    <textarea value={body} onChange={onBodyChange}></textarea>
+    <button>Do it</button>
+  </form>
+);
+
+Request.propTypes = {
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  onUrlChange: PropTypes.func.isRequired,
+  onMethodChange: PropTypes.func.isRequired,
+  onBodyChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
+
+export default Request;
